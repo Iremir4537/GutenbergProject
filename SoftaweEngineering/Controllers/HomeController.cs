21d5fefb6f7ae3c2d34c0e9bdf8fae32ef3904ac
Emir Tüfekci <emirtufekci4537@gmail.com>
2022-12-15 23:11:07 +0300

    FirsCommit

--- SoftaweEngineering/Controllers/HomeController.cs
+++ SoftaweEngineering/Controllers/HomeController.cs
@@ -1,21 +1,76 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using SoftaweEngineering.Models;
 using System.Diagnostics;
+using System.Xml;
+using System.Net.Http;
+using System.Net;
+using HtmlAgilityPack;
+using SoftaweEngineering.Models;
+using System.Web;
+using System.Text.RegularExpressions;
+using System.Text;
 
 namespace SoftaweEngineering.Controllers
 {
     public class HomeController : Controller
     {
+
         private readonly ILogger<HomeController> _logger;
+        static readonly HttpClient client = new HttpClient();
 
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
             return View();
+        }
+
+        public Book GetBook(string url, string urlN, int id)
+        {
+            string url1 = url + urlN;
+            HtmlWeb web = new HtmlWeb();
+            HtmlDocument doc = web.Load(url1);
+            List<string> list = new List<string>();
+            foreach (var item in doc.DocumentNode.SelectNodes("//head/style"))
+            {
+                string str = Regex.Replace(HttpUtility.HtmlEncode(item.InnerHtml.Trim()), @"\s", "");
+                str = str + "div.fig{margin-left:auto!important;margin-right:auto!important}";
+                list.Add(str);
+
+            }
+            foreach (var item in doc.DocumentNode.Descendants("img"))
+            {
+                string str1 = item.GetAttributeValue("src", null);
+                string str2 = url + str1;
+                item.SetAttributeValue("src", str2);
+            }
+
+            if (doc.DocumentNode.SelectSingleNode("//body/section[@class='pg-boilerplate pgheader']") != null)
+            {
+                doc.DocumentNode.SelectNodes("//body/section[@class='pg-boilerplate pgheader']")[0].Remove();
+                doc.DocumentNode.SelectNodes("//body/section[@class='pg-boilerplate pgheader']")[0].Remove();
+            }
+
+
+
+            Book book = new Book
+            {
+                Id = id,
+                Title = "title",
+                Styles = list,
+                Body = doc.DocumentNode.SelectSingleNode("//body").InnerHtml.Trim(),
+            };
+            return book;
+        }
+
+        [Route("book/{id:int}")]
+        public IActionResult Book(int id)
+        {
+            Book book = GetBook("https://www.gutenberg.org/cache/epub/"+id+"/", "pg"+id+ "-images.html",id);
+            return View(book);
         }
 
         public IActionResult Privacy()
---
+++ SoftaweEngineering/Models/Book.cs
@@ -0,0 +1,12 @@
+﻿using HtmlAgilityPack;
+namespace SoftaweEngineering.Models    
+
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public List<String> Styles { get; set; }
+        public String Body { get; set; }
+    }
+}
